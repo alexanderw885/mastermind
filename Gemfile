886748c1881f3fb3770c40420d49257b6cdea77e
Alexander Williams <alexanderw885@gmail.com>
2026-08-12 18:55:40 -0700

    add intro message and code colors

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem "rubocop", "~> 1.89"
+
+gem "colorize", "~> 1.1"
--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,27 @@
 # frozen_string_literal: true
+
+# Game stores the game objects and handles logic
+class Game
+  def initialize
+    @breaker = Breaker.new
+    @board = Board.new
+  end
+
+  def create_code
+    4.times { |i| @board.code[i] = Game.colors[Random.rand(6)] }
+  end
+
+  def self.colors
+    %i[red blue green yellow magenta white]
+  end
+
+  def print_intro
+    message = "Welcome to Mastermind!\nWe have #{Game.colors.length} colors:\n"
+    Game.colors.each { |c| message += "■ #{c}\n".colorize(c) }
+    message += "\nThere are also two types of info keys:\n"
+    message += "#{'■ white'.colorize(:white)}: 1 guess is the right color, wrong position\n"
+    message += "#{'■ red'.colorize(:red)}: 1 guess is the right color, right position\n\n"
+    message += "Guess by inputting the first letter of each color\n"
+    print message
+  end
+end
